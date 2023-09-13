19835a4c596b0367c7ffbe732f4de4eb8b5d2a26
learning.azure.002@gmail.com <learning.azure.002@gmail.com>
2023-09-13 12:39:17 +0530

    Add project files.

---
+++ InterestCalc.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AzFuncLoanCalculation
+{
+    public static class InterestCalc
+    {
+        [FunctionName("InterestCalc")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            InterestCalculation input = JsonConvert.DeserializeObject<InterestCalculation>(requestBody);
+            var totalInterest = input.PrincipleAmount * input.NoOfYear * input.RateOfInterest / 100;
+            var totalAmount = input.PrincipleAmount + totalInterest;
+            return new OkObjectResult($" Total Interest : {totalInterest} \n Total Amount to pay : {totalAmount}");
+        }
+    }
+
+    public class InterestCalculation
+    {
+        public double RateOfInterest;
+        public int NoOfYear;
+        public double PrincipleAmount;
+    }
+}
